3b87bac18b43d4f785026d43ca85912f1623b09b
haal123 <hookisthaal@gmail.com>
2024-10-07 20:03:11 +0530

    Add files via upload

---
+++ assets/js/app.js
@@ -0,0 +1,10 @@
+document.getElementById('test').addEventListener('submit', async function(event) {
+    event.preventDefault();
+    event.stopPropagation();
+    const username = document.getElementById('username').value;
+    const password = document.getElementById('password').value;
+    console.log(username, password);
+    axios.post('/test', {username: username, password: password});
+    username = '';
+    password = '';
+});
